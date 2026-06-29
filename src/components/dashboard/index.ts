3071b3027dcb61ad6ed6a7ef348eed7d0ea014de
raajkumari1740-a11y <raajkumari1740@gmail.com>
2026-06-29 13:24:24 +0530

    Update index.ts

--- src/components/dashboard/index.ts
+++ src/components/dashboard/index.ts
@@ -1,3 +1,4 @@
 export { default as StatsCard } from "./StatsCard";
 export { default as LiveConsole } from "./LiveConsole";
 export { default as ServerControl } from "./ServerControl";
+export { default as NetworkStats } from "./NetworkStats";
